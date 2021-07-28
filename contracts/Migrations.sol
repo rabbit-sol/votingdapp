e30e850977d58b93dc65283b8eedde575e68af61
SUBHAM KUNWAR <68282784+whiterabbit-hub@users.noreply.github.com>
2021-07-28 21:47:03 +0530

    Update Migrations.sol

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,43 +1,23 @@
 pragma solidity ^0.5.16;
- 
-contract Election {
-	// candidate structure
-	struct Candidate {
-		uint id;
-		string name;
-		uint voteCount;
-	}
-	
-	 // The mapping of candidate id to structure
-	mapping(uint => Candidate) public candidates;
- 
-	 // The mapping of voter address to whether to vote
-	mapping(address => bool) public voters;
-	
-	 // How many candidates in total
-	uint public candidatesCount;
-	
-	 	//Constructor 
-	constructor() public { 
-		addCandidate("Candidate 1");
-		addCandidate("Candidate 2");
-	}
-	
-	 // Add candidate
-	function addCandidate(string memory _name) private {
-		candidatesCount ++;
-		candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
-	}
- 
-	 // voting function
-	function vote(uint _candidateId) public {
-		 // Require voters to never vote
-		 require (! voters [msg.sender]); //msg.sender is the account that calls this function
-		 // Require the candidate Id to be legal
-		require(_candidateId > 0 &&_candidateId <= candidatesCount);
-		 // Confirm vote
-		voters[msg.sender] = true;
-		 // Update candidate votes
-		candidates[_candidateId].voteCount ++;
-	}
+
+contract Migrations {
+  address public owner;
+  uint public last_completed_migration;
+
+  modifier restricted() {
+    if (msg.sender == owner) _;
+  }
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  function setCompleted(uint completed) restricted public {
+    last_completed_migration = completed;
+  }
+
+  function upgrade(address new_address) restricted public {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
+  }
 }
